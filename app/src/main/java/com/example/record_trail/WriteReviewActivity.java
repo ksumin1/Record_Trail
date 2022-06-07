27672b9fecaa115887600d96dc0cbcbdd6dd1b78
sumin <w2005@e-mirim.hs.kr>
2022-06-08 08:28:06 +0900

    write_review 기능구현

--- app/src/main/java/com/example/record_trail/WriteReviewActivity.java
+++ app/src/main/java/com/example/record_trail/WriteReviewActivity.java
@@ -1,14 +1,41 @@
 package com.example.record_trail;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
 
-public class WriteReviewActivity extends AppCompatActivity {
+public class WriteReviewActivity extends Activity implements View.OnClickListener {
+
+    private int id_view;
+    private static final int PICK_FROM_ALBUM =1;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_write_review);
     }
+
+    public void doTakeAlbumAction(){
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
+        startActivityForResult(intent, PICK_FROM_ALBUM);
+    }
+
+    @Override
+    public void onClick(View v) {
+        id_view = v.getId();
+        if(v.getId() == R.id.UploadIMG){
+            DialogInterface.OnClickListener albumListener = new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    doTakeAlbumAction();
+                }
+            };
+        }
+    }
+
+
 }
